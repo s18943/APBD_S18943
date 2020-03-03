122dbdf386f78064de29b40525f92118f8c785cc
Serhii Krasnoiarskyi <s18943@pjwstk.edu.pl>
2020-03-03 11:23:13 +0100

    changes

---
+++ Cw1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var url = Console.ReadLine();
+
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetStringAsync(url);
+
+            //Console.WriteLine(content);
+
+            string resultes = string.Join(",",ExtractEmails(content));
+            Console.WriteLine(resultes);
+        }
+
+        public static string[] ExtractEmails(string str)
+        {
+            string RegexPattern = @"\b[A-Z0-9._-]+@[A-Z0-9][A-Z0-9.-]{0,61}[A-Z0-9]\.[A-Z.]{2,6}\b";
+
+            // Find matches
+            System.Text.RegularExpressions.MatchCollection matches
+                = System.Text.RegularExpressions.Regex.Matches(str, RegexPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            string[] MatchList = new string[matches.Count];
+
+            // add each match
+            int c = 0;
+            foreach (System.Text.RegularExpressions.Match match in matches)
+            {
+                MatchList[c] = match.ToString();
+                c++;
+            }
+
+            return MatchList;
+        }
+    }
+}
